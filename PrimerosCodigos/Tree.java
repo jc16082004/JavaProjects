d9c1acd07e5a87cf80deb812a8229b5811b6e64d
jc16082004 <143001678+jc16082004@users.noreply.github.com>
2023-09-14 09:16:20 -0500

    Create Tree.java

---
+++ PrimerosCodigos/Tree.java
@@ -0,0 +1,16 @@
+class tree
+  {
+    public static void main(String argas[]){
+      System.out.println("Hey there, I’m Henry and I’m learning to code in Java!");
+      System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+      System.out.println("   *   ");
+      System.out.println("  ***  ");
+      System.out.println("*******");
+      System.out.println(" *****  ");
+      System.out.println("  ***  ");
+      System.out.println("   *  ");
+      System.out.println("   *  ");
+      System.out.println("   *  ");
+      System.out.println("   *  ");
+   }  
+}  
