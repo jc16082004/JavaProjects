b87f4543235cf9c5bdf40ce94d6468518a9a4050
jc16082004 <143001678+jc16082004@users.noreply.github.com>
2023-11-08 09:39:26 -0500

    Add files via upload

---
+++ Segundo corte/Abstracta/Cuadrado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstracta1;
+
+public class Cuadrado extends FiguraGeometrica {
+
+    public Cuadrado(String tipoFigura) {
+        super(tipoFigura);
+    }
+    
+    @Override
+    public void dibujar() {
+        System.out.println("Se dibuja un: " + this.getClass().getSimpleName());
+    
+    }    
+}
